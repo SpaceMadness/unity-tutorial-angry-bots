465cac2669fe63d31e4fb5878a875d68f24727a1
Alex Lementuev <a.lementuev@gmail.com>
2015-07-23 11:59:36 -0700

    Added 'checkpoint' command

---
+++ Assets/Editor/Commands.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LunarPlugin;
+
+[CCommand("checkpoint")]
+class Cmd_checkpoint : CCommand
+{
+	void Execute(string name)
+	{
+		// 1. find checkpoint
+		// 2. find player
+		// 3. move player to checkpoint (if play mode)
+	}
+}
